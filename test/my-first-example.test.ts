a3d0cc317f627471310b098f5e8d3e5acdaa1c30
Khôi <nbkhoi@tma.com.vn>
2023-04-24 11:03:43 +0700

    Initial commit

---
+++ test/my-first-example.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as MyFirstExample from '../lib/my-first-example-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/my-first-example-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new MyFirstExample.MyFirstExampleStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
